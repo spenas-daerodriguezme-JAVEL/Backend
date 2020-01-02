dacddf6dfa026b5c49701b68060aa7f1a881cdc2
Santiago Peña <spenas@unal.edu.co>
2020-01-02 16:05:44 -0500

    burned variables for database

--- startup/db.ts
+++ startup/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default function() {
   mongoose
-    .connect("mongodb://mongo:27017/javel", {
+    .connect("mongodb://spenas:casa@db:27017/javel", {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true
