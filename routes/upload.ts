ddbae87a912aff5ff619e0fc397cf6b6b7501337
Santiago Peña <spenas@unal.edu.co>
2020-01-02 14:29:40 -0500

    commented upload for compiling

--- routes/upload.ts
+++ routes/upload.ts
@@ -9,49 +9,49 @@ const router = express()
 
 router.use(fileUpload());
 
-router.put('/upload/product/:id', function(req, res){
-    let tipo = req.params.tipo;
-    let id = req.params.id;
+// router.put('/upload/product/:id', function(req, res){
+//     let tipo = req.params.tipo;
+//     let id = req.params.id;
 
-    if(!req.files){
-        return res.status(400)
-            .json({
-                ok: false,
-                err: {
-                    message: 'None files were selected'
-                }
-            });
+//     if(!req.files){
+//         return res.status(400)
+//             .json({
+//                 ok: false,
+//                 err: {
+//                     message: 'None files were selected'
+//                 }
+//             });
 
-    }
-
-
-let images = req.files.images;
-let extFile = images.name.split('.');
-
-let validExtensions = ['png', 'jpg', 'jpeg'];
-
-if(validExtensions.indexOf(extFile) < 0){
-    return res.status(400).json({
-        ok: false,
-        err:{
-            message: 'Valid file extensions are: ' + validExtensions,
-            ext: extFile
-        }
-    })
-}
-
-images.mv(`images/product/${extFile}`, (err: any) => {
-    if(err)
-        return res.status(500).json({
-            ok:false,
-            err
-        });
+//     }
 
 
-});
+// let images = req.files.images;
+// let extFile = images.name.split('.');
+
+// let validExtensions = ['png', 'jpg', 'jpeg'];
+
+// if(validExtensions.indexOf(extFile) < 0){
+//     return res.status(400).json({
+//         ok: false,
+//         err:{
+//             message: 'Valid file extensions are: ' + validExtensions,
+//             ext: extFile
+//         }
+//     })
+// }
+
+// images.mv(`images/product/${extFile}`, (err: any) => {
+//     if(err)
+//         return res.status(500).json({
+//             ok:false,
+//             err
+//         });
 
 
-})
+// });
+
+
+// })
 
 export default {
     router
