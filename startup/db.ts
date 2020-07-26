f72e1cc930633091e2a9e8306fffc70365b6b87b
Data Science INGETEC <datascience@ingetec.com.co>
2020-07-26 09:23:23 -0500

    changed db connection

--- startup/db.ts
+++ startup/db.ts
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 
-const dbLocation = process.env.DB_CONTAINER || 'localhost';
+let stringConnection = '';
+if (process.env.APP_ENV === 'prod') {
+  stringConnection = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.DB_CONTAINER}:27017/${process.env.MONGODB_DATABASE}`;
+} else {
+  stringConnection = 'mongodb://localhost:27017/javel';
+}
+
 export default () => {
   mongoose
-    .connect(`mongodb://${dbLocation}:27017/javel`, {
+    .connect(stringConnection, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
