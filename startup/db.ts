e9070b097081fa96a51b6f502f87c160a00bf67c
Santiago Pena <spenas@unal.edu.co>
2020-11-14 07:39:45 -0500

    changed string connectyion

--- startup/db.ts
+++ startup/db.ts
@@ -2,10 +2,9 @@ import mongoose from 'mongoose';
 
 let stringConnection = '';
 if (process.env.APP_ENV === 'prod') {
-  stringConnection = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:27017/${process.env.MONGODB_DATABASE}`;
+  stringConnection = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 } else {
-  // stringConnection = 'mongodb://127.0.0.1:27017/javel';
-  stringConnection = 'mongodb://mongo/javel';
+  stringConnection = 'mongodb://127.0.0.1:27017/javel';
 }
 
 export default () => {
