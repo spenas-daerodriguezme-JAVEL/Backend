15bb9d463965262b5b3fe7b5c05375b9bc873edf
Santiago Pena <spenas@unal.edu.co>
2020-11-24 17:54:18 -0500

    https enabled

--- index.ts
+++ index.ts
@@ -7,10 +7,10 @@ import routes from './startup/routes';
 import db from './startup/db';
 import config from './startup/config';
 // TODO: Remove cors using env variable so that production env ignores it
-// const options = {
-//   key: fs.readFileSync(path.resolve('./build/certs/privkey.pem'), 'utf-8'),
-//   cert: fs.readFileSync(path.resolve('./build/certs/fullchain.pem'), 'utf-8'),
-// };
+const options = {
+  key: fs.readFileSync(path.resolve('./build/certs/privkey.pem'), 'utf-8'),
+  cert: fs.readFileSync(path.resolve('./build/certs/fullchain.pem'), 'utf-8'),
+};
 const app = express();
 app.use(cors());
 routes(app);
@@ -21,6 +21,6 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// https.createServer(options, app).listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
+https.createServer(options, app).listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
