7e575bb6b8ee4b3f0c15ae68fe8d5d81de048029
Santiago Pena <spenas@unal.edu.co>
2020-11-25 17:42:45 -0500

    chnages in multiples environments

--- index.ts
+++ index.ts
@@ -6,21 +6,24 @@ import cors from 'cors';
 import routes from './startup/routes';
 import db from './startup/db';
 import config from './startup/config';
-// TODO: Remove cors using env variable so that production env ignores it
-const options = {
-  key: fs.readFileSync(path.resolve('./build/certs/privkey.pem'), 'utf-8'),
-  cert: fs.readFileSync(path.resolve('./build/certs/fullchain.pem'), 'utf-8'),
-};
+
 const app = express();
 app.use(cors());
 routes(app);
 db();
 config();
 const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
 
-https.createServer(options, app).listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+if (process.env.APP_ENV === 'prod') {
+  const options = {
+    key: fs.readFileSync(path.resolve('./build/certs/privkey.pem'), 'utf-8'),
+    cert: fs.readFileSync(path.resolve('./build/certs/fullchain.pem'), 'utf-8'),
+  };
+  https.createServer(options, app).listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
